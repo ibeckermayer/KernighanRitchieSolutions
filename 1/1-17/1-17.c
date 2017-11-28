4cf795dfc0942ae2fed52054848fec42607d1d3f
ibeckermayer <ibeckermayer@gmail.com>
2017-11-27 16:07:39 -0800

    committing 1-17

---
+++ 1/1-17/1-17.c
@@ -0,0 +1,44 @@
+/* code stolen from http://clc-wiki.net/wiki/K%26R2_solutions:Chapter_1:Exercise_16 */
+
+#include <stdio.h>
+#define MAXLENGTH 1000
+#define LIMIT 80
+int getline2(char [],int);
+void copy(char [],char []);
+int main()
+{
+    int len = 0;
+    char line[MAXLENGTH],over80[MAXLENGTH];
+    while((len=getline2(line,MAXLENGTH))>0)
+        if(len>LIMIT){
+            copy(over80,line);
+	    printf("%s", over80);
+        }
+    printf("\n");
+    return 0;
+}
+
+int getline2(char line[],int limit)
+{
+    int i,c;
+    for(i=0;i<limit-1&&(((c=getchar())!=EOF)&&(c!='\n'));i++)
+        line[i]=c;
+    if(i==(limit-1)){
+        while((c=getchar())!='\n'){
+            ++i;
+        }
+    }
+    if(c=='\n'){        
+        line[i]=c;
+        ++i;
+    }
+    line[i]='\0';       
+    return i;
+}
+
+void copy(char to[],char from[])
+{
+    int i=0;
+    while((to[i]=from[i])!='\0')
+        ++i;
+}
