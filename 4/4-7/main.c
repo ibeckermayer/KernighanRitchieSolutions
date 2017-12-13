0d82e6a17df90f3640a9522b860721807b2c9cfa
ibeckermayer <ibeckermayer@gmail.com>
2017-12-12 18:45:22 -0800

    committing 4-7

---
+++ 4/4-7/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "calc.h"
+#define MAXOP 100
+int main()
+{
+  int type;
+  double op2;
+  char s[MAXOP];
+
+  while ((type = getop(s)) != EOF) {
+    switch (type) {
+    case NUMBER:
+      push(atof(s));
+      break;
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() * pop());
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 != 0.0)
+	push(pop() / op2);
+      else
+	printf("error: zero divisor\n");
+      break;
+    case '%':
+      op2 = pop();
+      if (op2 != 0.0)
+	push((int)pop() % (int)op2);
+      else
+	printf("error: zero divisor\n");
+      break;
+    case 128:
+      push(sin(pop()));
+      break;
+    case 129:
+      push(cos(pop()));
+      break;
+    case 130:
+      push(exp(pop()));
+      break;
+    case 131:
+      op2 = pop();
+      push(pow(pop(), op2));
+      break;
+    case '\n':
+      printf("\t%.8g\n", pop());
+      break;
+    default:
+      printf("error: unknown command %s\n", s);
+      break;
+    }
+  }
+  return 0;
+}
