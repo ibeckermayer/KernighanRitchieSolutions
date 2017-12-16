0af4fa073b59d7171159d82a702e209dc94546b1
Isaiah Beckermayer <320@p1-6.hbtn.io>
2017-12-16 15:30:34 -0800

    committing 5-8

---
+++ 5/5-8/5-8.c
@@ -0,0 +1,38 @@
+int main(){}
+
+static char daytab[2][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day)
+{
+  int i, leap;
+
+  leap = year%4 ==0 && year%100 !=0 || year %400 == 0;
+
+  if (year < 0 || month <0 || month > 12 || (day < 0 || day > daytab[leap][month-1])) {
+      printf("date invalid\n");
+      return -1;
+    }
+
+  for (i = 1; i < month; i++)
+    day += daytab[leap][i];
+  return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pda)
+{
+  int i, leap;
+
+  leap = year%4 ==0 && year%100 !=0 || year %400 == 0;
+  if (year < 0 || yearday < 0 || (leap ? yearday > 366 : yearday > 365)) {
+      printf("date invalid\n");
+      return;
+  }
+
+  for (i = 1; yearday > daytab[leap][i]; i++)
+    yearday -= daytab[leap][i];
+  *pmonth = i;
+  *pda = yearday;
+}
